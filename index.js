cf68752f02381ede39294bdb629932608d6d2dfb
Anthony Febus <febus.anthony@gmail.com>
2018-08-29 15:34:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ function sayHiToGrandma (string) {
   if (string === string.toLowerCase()){
     return "I can't hear you!";
       }
-      
+  if (string === string.toUpperCase()){
+    return "YES INDEED!";
+      }
       
       
       
